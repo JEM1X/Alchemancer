dc5d2799c1f0958cabb3c8e57c5e16192153c7a6
JEM1X <62144383+JEM1X@users.noreply.github.com>
2025-04-04 23:48:04 +0300

    0.16
    гайд + на будущие

--- Assets/UI Toolkit/Srcipts/UI/CombatUI.cs
+++ Assets/UI Toolkit/Srcipts/UI/CombatUI.cs
@@ -4,7 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
-public class MainUI : MonoBehaviour
+public class CombatUI : MonoBehaviour
 {
     [Header("Scene")]
     [SerializeField] private Camera mainCamera;
@@ -17,13 +17,16 @@ public class MainUI : MonoBehaviour
     [SerializeField] private UICombatStyle_SO combatStyle;
     [SerializeField] private UIAudioStyle_SO audioStyle;
 
-    private VisualElement handUI;
-    private VisualElement beltUI;
     private VisualElement hordeUI;
+    private VisualElement handUI;
+    private VisualElement bagUI;
+    private VisualElement beltUI;
+    private VisualElement interactionUI;
 
     private List<IngredientCard> cardsInCauldron = new List<IngredientCard>(0);
     private PotionCard potionInUse = null;
     private List<CombatantCard> enemyCards;
+    private bool isHandVisible = true;
 
     private void Awake()
     {
@@ -33,6 +36,14 @@ public class MainUI : MonoBehaviour
         mediator.PlayerHand.OnNewPotion += InitializePotionCard;
         mediator.PlayerCombat.OnSpawn += (Combatant combatant) => InitializePlayer((PlayerCombat)combatant);
         mediator.Horde.OnNewEnemy += InitializeEnemy;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            ToggleHand();
+        }
     }
 
     private void InitializeUI()
@@ -49,16 +60,22 @@ public class MainUI : MonoBehaviour
 
         handUI = UITK.AddElement(canvas, "handUI");
 
-        beltUI = UITK.AddElement(canvas, "beltUI");
+        interactionUI = UITK.AddElement(handUI, "interactionUI");
 
-        var endTurnButton = UITK.AddElement<Button>(canvas, "endTurnButton", "MainButton");
+        bagUI = UITK.AddElement(handUI, "bagUI");
+
+        beltUI = UITK.AddElement(handUI, "beltUI");
+
+
+
+        var endTurnButton = UITK.AddElement<Button>(interactionUI, "endTurnButton", "MainButton");
         endTurnButton.text = "Завершить Ход";
         endTurnButton.clicked += () => audioSource.PlayOneShot(audioStyle.SoundList[0]);
         endTurnButton.clicked += ClearCauldron;
-        endTurnButton.clicked += handUI.Clear;
+        endTurnButton.clicked += bagUI.Clear;
         endTurnButton.clicked += mediator.PlayerHand.DrawNewHand;
 
-        var brewButton = UITK.AddElement<Button>(canvas, "brewButton", "MainButton");
+        var brewButton = UITK.AddElement<Button>(interactionUI, "brewButton", "MainButton");
         brewButton.text = "Сварить";
         brewButton.clicked += BrewPotion;
     }
@@ -68,7 +85,7 @@ public class MainUI : MonoBehaviour
         var ingredientCard = new IngredientCard(ingredient);
         ingredientCard.cardFrame.clicked += () => UseIngredientCard(ingredientCard);
 
-        handUI.Add(ingredientCard.cardFrame);
+        bagUI.Add(ingredientCard.cardFrame);
 
         ingredientCard.cardFrame.RegisterCallback<PointerEnterEvent>(evt =>
             audioSource.PlayOneShot(audioStyle.SoundList[3]));
@@ -116,30 +133,9 @@ public class MainUI : MonoBehaviour
         }
     }
 
-    private void BrewPotion()
-    {
-        if (cardsInCauldron.Count < 3) return;
-        if (beltUI.childCount >= 3) return;
-
-        Ingredient_SO[] usedIngredients = cardsInCauldron.Select(card => card.ingredient).ToArray();
-
-        mediator.PlayerHand.BrewNewPotion(usedIngredients);
-
-        cardsInCauldron.ForEach(card => card.cardFrame.RemoveFromHierarchy());
-
-        ClearCauldron();
-
-        audioSource.PlayOneShot(audioStyle.SoundList[4]);
-    }
-
-    private void ClearCauldron()
-    {
-        cardsInCauldron = new List<IngredientCard>(0);
-    }
-
     private void UsePotionCard(PotionCard card)
     {
-        if(card.potion is Elixir_SO elixir)
+        if (card.potion is Elixir_SO elixir)
         {
             mediator.PlayerHand.UseElixir(elixir);
             card.cardFrame.RemoveFromHierarchy();
@@ -147,7 +143,7 @@ public class MainUI : MonoBehaviour
             return;
         }
 
-        if(card.potion is Flask_SO flask)
+        if (card.potion is Flask_SO flask)
         {
             mediator.PlayerHand.UseFlask(flask);
             card.cardFrame.RemoveFromHierarchy();
@@ -170,6 +166,27 @@ public class MainUI : MonoBehaviour
         }
     }
 
+    private void BrewPotion()
+    {
+        if (cardsInCauldron.Count < 3) return;
+        if (beltUI.childCount >= 3) return;
+
+        Ingredient_SO[] usedIngredients = cardsInCauldron.Select(card => card.ingredient).ToArray();
+
+        mediator.PlayerHand.BrewNewPotion(usedIngredients);
+
+        cardsInCauldron.ForEach(card => card.cardFrame.RemoveFromHierarchy());
+
+        ClearCauldron();
+
+        audioSource.PlayOneShot(audioStyle.SoundList[4]);
+    }
+
+    private void ClearCauldron()
+    {
+        cardsInCauldron = new List<IngredientCard>(0);
+    }
+
     private void AttackEnemy(Enemy enemy)
     {
         if (potionInUse?.potion is Capsule_SO capsule)
@@ -178,6 +195,22 @@ public class MainUI : MonoBehaviour
             potionInUse.cardFrame.RemoveFromHierarchy();
             potionInUse = null;
             audioSource.PlayOneShot(audioStyle.SoundList[7]);
+        }
+    }
+
+    private void ToggleHand()
+    {
+        if (isHandVisible)
+        {
+            handUI.SetEnabled(false);
+            handUI.pickingMode = PickingMode.Ignore;
+            isHandVisible = false;
+        }
+        else
+        {
+            handUI.SetEnabled(true);
+            handUI.pickingMode = PickingMode.Position;
+            isHandVisible = true;
         }
     }
 }
---
+++ Assets/UI Toolkit/Srcipts/UI/InGameGuide.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class InGameGuide : MonoBehaviour
+{
+    [SerializeField] private UIDocument uiDocument;
+    [SerializeField] private UIStyle_SO styleSheet;
+
+    private VisualElement background;
+    private bool isVisible = true;
+
+
+    private void Awake()
+    {
+        InitializeUI();
+    }
+
+    private void InitializeUI()
+    {
+        VisualElement root = uiDocument.rootVisualElement;
+        root.Clear();
+
+        foreach (StyleSheet sheet in styleSheet.styles)
+            root.styleSheets.Add(sheet);
+
+        var canvas = UITK.AddElement(root, "canvas", "MainText");
+
+        background = UITK.AddElement(canvas, "background");
+
+        var guideFrame = UITK.AddElement(background, "guideFrame");
+
+        var guidePage = UITK.AddElement(guideFrame, "guidePage");
+
+        var pageLabel = UITK.AddElement<Label>(guidePage, "pageLabel");
+        pageLabel.text = "Вы — кобольд - алхимик, которому предстоит пробиться сквозь толпы монстров." +
+            "Для победы вам нужно пройти все этапы, каждый из которых состоит из трёх волн врагов." +
+            "С каждым новым этапом сложность будет увеличиваться," +
+            "и вам придётся использовать всю свою алхимическую сноровку, чтобы выжить." +
+            "В бою вам помогут зелья, которые делятся на три вида: Эликсиры, Капсулы и Колбы." +
+            "Эликсиры применяются на игрока, Капсулы воздействуют на одного врага, а Колбы поражают всех врагов сразу." +
+            "Однако в начале каждого этапа у вас не будет ни одного зелья.";
+
+        var guidePage1 = UITK.AddElement(guideFrame, "guidePage");
+
+        var page1Label = UITK.AddElement<Label>(guidePage1, "pageLabel");
+        page1Label.text = "Чтобы их создать, вам нужно правильно смешивать ингредиенты." +
+            "Каждое зелье имеет уникальный рецепт, состоящий из трёх разных ингредиентов." +
+            "В начале вашего хода вам будет выдано шесть карт ингредиентов." +
+            "Используйте их, чтобы создавать зелья и применять их в бою." +
+            "Когда у вас закончатся ингредиенты и зелья, нажмите 'Завершить ход' — тогда ход перейдёт к вашим противникам." +
+            "После того как все враги завершат свои ходы, начнётся новый раунд, и ход снова перейдёт к вам." +
+            "Ваша цель — победить всех врагов и не погибнуть на пути. Удачи!";
+
+        var toggleButton = UITK.AddElement<Button>(canvas, "toggleButton");
+        toggleButton.text = "Гайд";
+        toggleButton.clicked += ToggleGuide;
+
+        ToggleGuide();
+    }
+
+    private void ToggleGuide()
+    {
+        if (isVisible)
+        {
+            background.style.display = DisplayStyle.None;
+            isVisible = false;
+        }
+        else
+        {
+            background.style.display = DisplayStyle.Flex;
+            isVisible = true;
+        }
+    }
+}
